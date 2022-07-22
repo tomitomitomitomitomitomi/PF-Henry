7b86f9cb37856f5341dcd1fff42625c10377594c
Juan Codina <91763407+jcodina1@users.noreply.github.com>
2022-07-22 07:19:41 -0500

    Delete api directory

--- api/src/routes/SellerRoutes.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require('express');
-const { getAllSellers, postSellers } = require('../controllers/SellerController');
-
-const router = Router();
-
-router.post('/', postSellers)
-router.get('/', getAllSellers)
-
-module.exports = router
